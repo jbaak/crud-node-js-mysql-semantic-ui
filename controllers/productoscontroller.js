f3b46bd7437a26094867923f17024732ed31448d
Jesus Baak <jbbaak@gmail.com>
2017-03-10 23:33:14 -0600

    Se inicializo proyecto, se creo plantilla, se conecto a mysql y creo vista para mostrar productos

---
+++ controllers/productoscontroller.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+module.exports={
+	
+	getProductos:function(req, res, next){
+		var config = require('.././database/config');
+		var db = mysql.createConnection(config);
+		db.connect();
+
+		 var productos=null;
+
+		db.query('select * from productos', function(err, rows, fields){
+			if(err) throw err;
+			productos =rows;
+			db.end();
+			res.render('productos/productos', {productos: productos});
+		});
+
+		
+	}
+}
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var controllers = require('.././controllers');
+
+/* GET home page. */
+router.get('/', controllers.homecontroller.index);
+
+//rutas para productos
+router.get('/productos', controllers.productoscontroller.getProductos);
+
+module.exports = router;
