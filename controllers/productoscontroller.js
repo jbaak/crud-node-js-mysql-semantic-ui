55381947185cc03ce1032249946dbfd32e5306f2
Jesus Baak <jbbaak@gmail.com>
2017-03-11 17:53:04 -0600

    Sea agrego formulario y metodos en el controlador para agregar nuevo producto

--- controllers/productoscontroller.js
+++ controllers/productoscontroller.js
@@ -1,21 +1,25 @@
 var mysql = require('mysql');
 
 module.exports={
-	
-	getProductos:function(req, res, next){
-		var config = require('.././database/config');
-		var db = mysql.createConnection(config);
-		db.connect();
+    getProductos:function(req, res, next){
+        var config = require('.././database/config');
+        var db = mysql.createConnection(config);
+        db.connect();
 
-		 var productos=null;
+         var productos=null;
 
-		db.query('select * from productos', function(err, rows, fields){
-			if(err) throw err;
-			productos =rows;
-			db.end();
-			res.render('productos/productos', {productos: productos});
-		});
+        db.query('select * from productos', function(err, rows, fields){
+            if(err) throw err;
+            productos =rows;
+            db.end();
+            res.render('productos/productos', {productos: productos});
+        });
+    },
+    getNuevoProducto: function(req, res, next){
+        res.render('productos/nuevo');
+    },
+    postNuevoProducto: function(req, res, next){
+        console.log(req.body);
+    },
 
-		
-	}
 }
--- routes/routes.js
+++ routes/routes.js
@@ -9,4 +9,9 @@ router.get('/', controllers.homecontroller.index);
 //rutas para productos
 router.get('/productos', controllers.productoscontroller.getProductos);
 
+router.get('/nuevo', controllers.productoscontroller.getNuevoProducto);
+
+router.post('/crearproducto', controllers.productoscontroller.postNuevoProducto);
+
+
 module.exports = router;
